4351bc16edaa412ff1b378a68fa8312db000d958
RobinBecard <robinbecard@gmail.com>
2025-12-11 18:22:13 -0500

    Adding models root to config.py

--- src/config.py
+++ src/config.py
@@ -3,3 +3,4 @@ from pathlib import Path
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 DATASET_PATH = PROJECT_ROOT / "datasets" / "neo.csv"
 RESULTS_DIR = PROJECT_ROOT / "results"
+MODELS_DIR = PROJECT_ROOT / "models"
