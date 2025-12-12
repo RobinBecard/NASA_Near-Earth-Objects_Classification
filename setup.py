dcc829f1fd143bdcd51fb3938a70ca2069a93b01
RobinBecard <robinbecard@gmail.com>
2025-12-12 15:11:13 -0500

    Update setup

--- setup.py
+++ setup.py
@@ -3,7 +3,15 @@ from setuptools import setup, find_packages
 setup(
     name="IFT712_Project",
     version="0.1.0",
-    package_dir={"": "src"},           
+    package_dir={"": "src"},
+    install_requires=[
+        "pandas",
+        "numpy",
+        "scikit-learn"
+    ],           
     packages=find_packages("src"),
+    author="Robin BECARD, Adrien SKRZYPCZAK, Cédric HAN",
+    author_email="robinbecard@gmail.com, auteur1@example.com, auteur2@example.com",
+    description="A project for IFT712, including machine learning components.",
 )
 
